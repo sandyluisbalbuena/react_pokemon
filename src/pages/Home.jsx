a0c2288c7f00331660a43bd09839a88dba6885e4
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-06-09 22:48:53 +0800

    wew

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react'
 const Home = () => {
 	let splide;
 	useEffect(()=>{
-		// initiate splide
 		if (window.innerWidth <= 768) {
 			splide = new Splide( '.splide', {
 				type   : 'loop',
@@ -59,7 +58,6 @@ const Home = () => {
 
 				let newSlide = document.createElement('li');
 				newSlide.className = 'splide__slide';
-				// newSlide.textContent = 'New Slide';
 
 				newSlide.appendChild(newAnchor);
 
@@ -69,17 +67,7 @@ const Home = () => {
 		})
 		.catch(error => console.error('On get pokemon error', error))
 		.then(() => { 
-			// $(document).ready(function(){
-				
-				// setTimeout(resolve, 3000);  
-			// });
-			// new Promise((resolve) => {
-			// 	loaderbackground.classList.add('animate__animated'); 
-			// 	loaderbackground.classList.add('animate__fadeOut');
-			// 	setTimeout(() => resolve(), 1000); // Delay execution by 3 seconds
-			// }).then(() => {
-			// 	loaderbackground.style.display = "none";
-			// });
+		
 		})
 	}
 
@@ -88,41 +76,41 @@ const Home = () => {
 
 
 
-  return (
-	<>
-	<div className="p-5 text-center bg-image hero">
-		<div className="mask">
-			<div className="d-flex justify-content-center align-items-center h-100">
-				<div className="text-white">
-					<h1 className="mb-3"><img src="src/assets/images/brand/pokemonBrandName2.png" width="40%"/></h1>
-					<form className="d-flex input-group w-auto mt-5 container" action="/pokedex" method="get">
-						<input id="pokemonName" name="pokemonName" type="search" className="form-control rounded" placeholder="Pokemon Search" aria-label="Search" aria-describedby="search-addon" required/>
-						<button className="btn bg-dark" type="submit">
-							<i className="fas fa-search text-white"></i>
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<main className="container-fluid">
-		<section>
-			<div className="row">
-				<div className="splide">
-					<div className="splide__track">
-						<ul className="splide__list">
-							
-						</ul>
+	return (
+		<>
+		<div className="p-5 text-center bg-image hero">
+			<div className="mask">
+				<div className="d-flex justify-content-center align-items-center h-100">
+					<div className="text-white">
+						<h1 className="mb-3"><img src="src/assets/images/brand/pokemonBrandName2.png" width="40%"/></h1>
+						<form className="d-flex input-group w-auto mt-5 container" action="/pokedex" method="get">
+							<input id="pokemonName" name="pokemonName" type="search" className="form-control rounded" placeholder="Pokemon Search" aria-label="Search" aria-describedby="search-addon" required/>
+							<button className="btn bg-dark" type="submit">
+								<i className="fas fa-search text-white"></i>
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
-		</section>
-	</main>
+		</div>
+
+		<main className="container-fluid">
+			<section>
+				<div className="row">
+					<div className="splide">
+						<div className="splide__track">
+							<ul className="splide__list">
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
 
 
-	</>
-  )
+		</>
+	)
 }
 
 export default Home
